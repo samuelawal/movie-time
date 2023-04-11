dcc9fabfcdffe00d75ca2173edaddcd9f44bd2b4
Samuel Lawal <samuellawal60@gmail.com>
2023-04-11 11:27:50 +0100

    fix: Add Api keys for data fetching

--- src/plugins/config.js
+++ src/plugins/config.js
@@ -1,4 +1,4 @@
 export const Config = {
-    apiKey: process.env.VUE_APP_MOVIE_DB_API_KEY,
-    apiToken: process.env.VUE_APP_MOVIE_DB_ACCESS_TOKEN
+    apiKey: "3a2cf5b3952891f0edb1dd5b965f9336",
+    apiToken: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYTJjZjViMzk1Mjg5MWYwZWRiMWRkNWI5NjVmOTMzNiIsInN1YiI6IjYyNDE5MmEwOTQ1MWU3MDA4YzkyZDlmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hml-5mWgiqTXoCq7FBkIXEj1EM2SMbbJN2ztfhhDSIM"
 }
